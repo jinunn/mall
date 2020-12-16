9dbca61aa653da984d224365351f49f5413eebe2
jinunn <372138750@qq.com>
2020-12-16 15:26:17 +0800

    项目初始结构创建

---
+++ mall-order/src/test/java/com/jinunn/mall/order/MallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinunn.mall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
